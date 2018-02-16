790b55bb094e14590e071d3982e9d43bf041e8d4
Lauren ten Hoor <laurentenhoor@gmail.com>
2018-02-17 00:21:57 +0100

    Running on both ios and android

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+// import { BLE } from '@ionic-native/ble';
 
 import { HomePage } from '../pages/home/home';
 @Component({
@@ -10,14 +11,14 @@ import { HomePage } from '../pages/home/home';
 export class MyApp {
   rootPage: any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {//, private ble: BLE
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      if (platform.is('cordova')) {
+      // if (platform.is('cordova')) {
         statusBar.styleDefault();
         splashScreen.hide();
-      }
+      // }
 
     });
   }
