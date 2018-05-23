95f66e614e75d984cc6ffd98dfccfb5f341bd278
Lauren ten Hoor <laurentenhoor@gmail.com>
2018-05-23 12:52:24 +0200

    Initial Dynamic Pairing Flow with Bluetooth Detection

--- src/pages/pairing/pairing.ts
+++ src/pages/pairing/pairing.ts
@@ -1,6 +1,14 @@
 
 import { ViewChild, Component, NgZone } from '@angular/core';
 import { AlertController, NavController, Slides, App, Events, ToastController, ModalController } from 'ionic-angular';
+import { Sleeves } from '../../providers/sleeves';
+
+
+enum PairStep {
+    PROPOSITION,
+    CONSENT,
+    PAIRING
+}
 
 @Component({
     selector: 'pairing',
@@ -9,6 +17,9 @@ import { AlertController, NavController, Slides, App, Events, ToastController, M
 export class Pairing {
 
     @ViewChild(Slides) slides: Slides;
+    private PairStep: typeof PairStep = PairStep;
+    private pairingTimeoutError: boolean = false;
+    private bluetoothActivatedError: boolean = false;
 
     constructor(
         private app: App,
@@ -17,7 +28,8 @@ export class Pairing {
         private events: Events,
         private alertCtrl: AlertController,
         private toastCtrl: ToastController,
-        private modalCtrl: ModalController
+        private modalCtrl: ModalController,
+        private sleevesService: Sleeves
     ) {
     }
 
@@ -25,10 +37,50 @@ export class Pairing {
         this.events.subscribe('sleeve-disconnected', () => {
             // do something if sleeve disconnects
         });
+
+        setTimeout(()=>{
+            this.slides.slideTo(2)
+        }, 200)
+        
+    }
+
+    ionViewDidEnter() {
+        this.checkBluetooth();
     }
     
     nextSlide() {
         this.slides.slideNext();
     }
 
+    closeModal() {
+        console.log('todo: implement closeModal()')
+    }
+
+    checkBluetooth() {
+        this.sleevesService.isBluetoothEnabled()
+            .then(() => {
+                console.log('Bluetooth is turned ON.')
+                this.bluetoothActivatedError = false;
+            }).catch(() => {
+                console.log('Bluetooth is turned OFF.')
+                this.bluetoothActivatedError = true;
+            })
+    }
+
+    showPairingErrorHints() {
+        console.log('todo implement pairing error hints')
+    }
+
+    showBluetoothErrorHints() {
+        console.log('todo implement pairing error hints')
+    }
+
+    showDeviceActionToast() {
+        let toast = this.toastCtrl.create({
+            message: 'Please Use Your Sleeve!',
+            duration: 3000,
+            position: 'bottom',
+        });
+        toast.present();
+    }
 }
