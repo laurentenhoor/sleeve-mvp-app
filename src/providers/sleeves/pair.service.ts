f7dcf2479d9705272104e795445dea2e59991a42
Lauren ten Hoor <laurentenhoor@gmail.com>
2018-05-25 16:24:51 +0200

    Pairing refactoring tested on web

--- src/providers/sleeves/pair.service.ts
+++ src/providers/sleeves/pair.service.ts
@@ -1,13 +1,12 @@
 import { Injectable } from '@angular/core';
 import { BLE } from '@ionic-native/ble';
-import { PairModel } from './pair.model';
 import { Events } from 'ionic-angular';
 import { ConnectService } from './connect.service';
-import { ThrowStmt } from '@angular/compiler';
+import { PairModel } from './pair.model';
 
 @Injectable()
 export class PairService {
-    private defaultSleeveName: string = 'Philips Avent SCH820';
+    private DEFAULT_SLEEVE_NAME: string = 'Philips Avent SCH820';
     private connectedDeviceId: string;
     private sleeveConnected: boolean = false;
 
@@ -24,22 +23,43 @@ export class PairService {
         return this.pairWithRetryMechanism();
     }
 
-    private async pairWithRetryMechanism(retryResolve?): Promise<any> {
+    private pairWithRetryMechanism(retryResolve?, retryReject?): Promise<any> {
         return new Promise((resolve, reject) => {
             if (!retryResolve) {
                 retryResolve = resolve;
+                retryReject = reject;
             }
+
             this.scanUntilFirstFoundSleeve().then((foundSleeve) => {
                 return this.connectService.connect(foundSleeve.id);
+
             }).then((connectedSleeve) => {
                 return this.forceBonding(connectedSleeve.id)
+
             }).then((bondedSleeve) => {
                 retryResolve(bondedSleeve);
+
             }).catch((error) => {
-                console.error(error);
-                this.pairWithRetryMechanism(resolve);
+                this.handlePairingErrors(error).then(
+                    (retry) => this.pairWithRetryMechanism(resolve, reject),
+                    (stop) => retryReject()
+                );
             })
 
+        })
+    }
+
+    private handlePairingErrors(error: string) {
+        console.log('Pairing error:', error)
+        return new Promise((resolve, reject) => {
+            switch (error) {
+                case 'cordova_not_available':
+                    reject('stop retrying pairing');
+                    break;
+                default:
+                    resolve('continue retrying pairing');
+                    break;
+            }
         })
     }
 
@@ -50,7 +70,7 @@ export class PairService {
         return new Promise((resolve, reject) => {
             this.ble.startScan([]).subscribe(
                 peripheral => {
-                    if (this.isAPhilipsSleeve(peripheral)) {
+                    if (this.isAPhilipsSleeve(peripheral.name)) {
                         this.ble.stopScan();
                         resolve(peripheral)
                     }
@@ -64,7 +84,7 @@ export class PairService {
     }
 
     private isAPhilipsSleeve(deviceName: string): boolean {
-        return deviceName == this.defaultSleeveName
+        return deviceName == this.DEFAULT_SLEEVE_NAME;
     }
 
     private forceBonding(deviceId: string): Promise<any> {
